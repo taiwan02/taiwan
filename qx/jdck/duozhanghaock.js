8c876c857360b1506448f56713f1f650d1c64baf
taiwan02 <76515928+taiwan02@users.noreply.github.com>
2021-09-27 20:21:50 +0800

    Create duozhanghaock.js

---
+++ qx/jdck/duozhanghaock.js
@@ -0,0 +1,4 @@
+hostname = ms.jr.jd.com, me-api.jd.com, api.m.jd.com, wq.jd.com
+
+# 获取多账号京东Cookie
+https:\/\/wq\.jd\.com\/user_new\/info\/GetJDUserInfoUnion url script-request-header https://raw.githubusercontent.com/taiwan02/taiwan/main/qx/jdck/JD_extra_cookie.js
