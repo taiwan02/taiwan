b888f308a4ee8668ee8a69243335458d4e579ad8
taiwan02 <76515928+taiwan02@users.noreply.github.com>
2021-09-28 12:55:02 +0800

    Create sq.sh

---
+++ ql/sq.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+cd /ql/ninja/backend
+pm2 start
+cd /ql/scripts
+sed -i 's/吹水群：https:\/\/t.me\/wenmouxx/苏青/g' shufflewzc_faker2_jd_qqxing.js
+sed -i 's/开源免费By: https:\/\/github.com\/curtinlv\/JD-Script/苏青/g' sendNotify.py
+sed -i 's/通知 By：https:\/\/github.com\/whyour\/qinglong/本通知 By：苏青/g' sendNotify.js
