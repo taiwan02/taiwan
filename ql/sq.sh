7d71a02c753860898cdd2f98d3d1f2b21888b11c
taiwan02 <76515928+taiwan02@users.noreply.github.com>
2021-10-02 01:49:40 +0800

    Update sq.sh

--- ql/sq.sh
+++ ql/sq.sh
@@ -4,4 +4,4 @@ pm2 start
 cd /ql/scripts
 sed -i 's/吹水群：https:\/\/t.me\/wenmouxx/苏青/g' shufflewzc_faker2_jd_qqxing.js
 sed -i 's/开源免费By: https:\/\/github.com\/curtinlv\/JD-Script/苏青/g' sendNotify.py
-sed -i 's/通知 By：https:\/\/github.com\/whyour\/qinglong/本通知 By：苏青/g' sendNotify.js
+sed -i 's/本通知 By：https:\/\/github.com\/whyour\/qinglong/本通知 By：苏青/g' sendNotify.js
